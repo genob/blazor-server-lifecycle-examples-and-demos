ee0082253ded816fea402e976484ec6ab0c7f3e5
Gene Bederak <gbederak@rockportllc.com>
2024-01-20 06:25:44 -0500

    initial

---
+++ BlazorDemos/LifecycleDemos/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorDemos.LifecycleDemos
+{
+    public static class Extensions
+    {
+        public static bool IsPreRendering(this IJSRuntime runtime)
+        {
+            // The peculiar thing in prerender is that Blazor circuit isn't yet created, so we can't use JSInterop
+            return !(bool)runtime.GetType().GetProperty("IsInitialized").GetValue(runtime);
+        }
+    }
+}
